f52a685212888d0bdfa7afd702b9171c9e85676b
Alain Rodriguez <arodrime@gmail.com>
2016-02-24 18:33:10 +0100

    Add Licence, description and author

--- remove_disk/remove_extra_disk.sh
+++ remove_disk/remove_extra_disk.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
+#
+# The MIT License (MIT)
+# Copyright (c) 2016 The Last Pickle
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#
+#
+#
+# Script description
+#
+# - The script stops the node, so should be run sequentially.
+# - It performs 2 more rsync:
+#   - The first one to take the diff between the end of 3rd rsync and the moment you stop the node, it should be a few seconds, maybe minutes, depending how fast the script was run after 3rd rsync ended and on the throughput.
+#   - The second rsync in the script is a 'control' one. I just like to control things. Running it, we expect to see that there is no diff. It is just a way to stop the script if for some reason data is still being appended to old-dir (Cassandra not stopped correctly or some other weird behavior). I guess this could be replaced/completed with a check on Cassandra service making sure it is down.
+# - Next step in the script is to move all the files from tmp-dir to new-dir (the proper data folder remaining after the operation). This is an instant operation as files are not really moved as they already are on the disk as mentioned earlier.
+# - Finally the script unmount the disk and remove the old-dir.
+#
+#
+#
+# Author: Alain Rodriguez, for The Last Pickle
+# Email: alain@thelastpickle.com
+
 # User defined variables
 path_old_disk=/var/lib/cassandra
 path_new_disk=/var/lib/cassandra_new
